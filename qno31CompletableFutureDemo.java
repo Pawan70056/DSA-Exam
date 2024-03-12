be71ea49eebfd5a4103b2a32bb52c77b8cea1c3c
Pawan70056 <ssmp70056@gmail.com>
2024-03-12 17:30:06 +0545

    remove

--- qno31CompletableFutureDemo.java
+++ qno31CompletableFutureDemo.java
@@ -8,14 +8,12 @@ import java.util.concurrent.CompletableFuture;
 public class qno31CompletableFutureDemo {
 
     public static void main(String[] args) {
-        // Simulating fetching data from two different sources asynchronously
+        
         CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> fetchData("Source 1"));
         CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> fetchData("Source 2"));
 
-        // Combining the results of both tasks when they are completed
         CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(future1, future2);
 
-        // Handling the combined result
         combinedFuture.thenRun(() -> {
             try {
                 String result1 = future1.get();
@@ -28,24 +26,19 @@ public class qno31CompletableFutureDemo {
             }
         });
 
-        // Program continues to run while tasks are executed asynchronously
         System.out.println("Fetching data from multiple sources asynchronously...");
-        // Other tasks can be performed while waiting for the completion of asynchronous tasks
         System.out.println("Performing other tasks while waiting...");
 
-        // Preventing the main thread from exiting before completion of asynchronous tasks
         try {
-            Thread.sleep(3000); // Simulating other tasks being performed
+            Thread.sleep(3000); 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    // Simulated method to fetch data from a source
     private static String fetchData(String source) {
-        // Simulating fetching data from a source
         try {
-            Thread.sleep(2000); // Simulating delay
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
